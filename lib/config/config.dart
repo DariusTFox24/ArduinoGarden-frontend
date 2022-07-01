06df77ee80eafb0296bdf3eec66f57c6c452a4a9
DESKTOP-NC888NK\dariu <darius.teperdel.mi@gmail.com>
2022-07-01 15:15:25 +0300

    changed url

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,5 @@
 import 'package:arduino_garden/config/api.dart';
 
-final Uri host = Uri.parse('http://oraclevm.xyn.ro:5003');
+final Uri host = Uri.parse('http://arduinogarden.server001.xyn.ro');
 // final Uri host = Uri.parse('http://oraclevm.xyn.ro:6210');
 final api = ArduinoGardenApi(host);
